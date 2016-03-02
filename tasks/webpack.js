a406d6151e47a1809ee528270207be9c92b80143
guoshencheng <648772021@qq.com>
2016-03-02 15:52:25 +0800

    add webpack

---
+++ tasks/webpack.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var gutil = require('gulp-util');
+var exec = require('child_process').exec;
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('../webpack.config.js');
+module.exports = function (gulp) {
+  gulp.task('webpack', function (callback) {
+      webpack(webpackConfig, function (err, stats) {
+            if (err) throw new gutil.PluginError('webpack', err);
+            gutil.log("[webpack]", stats.toString({
+            
+            }))
+      })
+  })
+   gulp.task('webpackDevServer', function(callback) {
+       console.log('starting webpack-dev');
+       exec('webpack-dev-server --hot --inline --display-error-details --port '+webpackConfig.webpackDevPort,function(){
+             console.log.apply(console,arguments);
+       })
+    })
+}
