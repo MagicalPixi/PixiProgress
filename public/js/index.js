86f93adb86aeb74f8af44eb17e27d0c5eb47c213
guoshencheng <648772021@qq.com>
2016-03-02 17:27:18 +0800

    add graphics style progress and prepare for custom

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,12 @@ var renderer = new PIXI.autoDetectRenderer(640, 1004, {
     transparent:true
   }
 )
+var Progress = require('./progress.js')
+var progress = Progress()
 document.body.appendChild(renderer.view)
-var stage = new PIXI.Container()
-
+var stage = new PIXI.Container({backgroundColor : 0x1099bb})
+stage.addChild(progress)
+progress.update(0.5)
 animate()
 function animate() {
   requestAnimationFrame(animate)
